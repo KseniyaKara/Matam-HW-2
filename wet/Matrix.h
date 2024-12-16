9ee4ccb0c57a667d8459d8bdb39d98382316618e
KseniyaKara <kseniya.kara2@gmail.com>
2024-12-16 19:15:49 +0200

    fix matrix class: erase "default" where it is not relevant

--- wet/Matrix.h
+++ wet/Matrix.h
@@ -1,13 +1,13 @@
 #pragma once
 
-class Matrix {
+class Matrix{
     unsigned int rowNum = 0;
     unsigned int colNum = 0;
     int* matrix = nullptr;
     
     public:
-        Matrix(unsigned rowNum = 0, unsigned colNum = 0, int initValue = 0) = default;
-        ~Matrix() = default;
+        Matrix(unsigned rowNum = 0, unsigned colNum = 0, int initValue = 0);
         Matrix(const Matrix& other);
-        Matrix& Matrix::operator=(const Matrix& s) = default;
+        Matrix& Matrix::operator=(const Matrix& s);
+        ~Matrix();
 };
