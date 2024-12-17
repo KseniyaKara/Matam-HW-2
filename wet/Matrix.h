a3a5ebb3c3afb324ac4e9071459f0a6d6049a261
KseniyaKara <kseniya.kara2@gmail.com>
2024-12-17 12:32:17 +0200

    add declaration of static functions and operators

--- wet/Matrix.h
+++ wet/Matrix.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <ostream>
 
 class Matrix{
     unsigned int rowNum = 0;
@@ -18,5 +19,21 @@ class Matrix{
         // void setRowNum(int row);
         const int& Matrix::operator()(int row, int col) const;
         friend bool operator==(const Matrix& matrix, const Matrix& other);
+        Matrix& operator*=(int scalar);
+        Matrix operator-() const;
+        Matrix& operator+=(const Matrix& other);
+        Matrix& operator-=(const Matrix& other);
+        Matrix& operator*=(const Matrix& other);
+        void sizeMatch(const Matrix& other) const;
+        friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
+        void Matrix::isSquare() const;
+        static int CalcDeterminant(const Matrix matrix);
+        static int CalcDeterminantRec(const Matrix& matrix, int row, int col, int* ignoredRowsMask, \
+        int* ignoredColumnsMask) ;
+        static int CalcSingleElementMult(const Matrix& matrix, const Matrix& other);
 };
     bool operator!=(const Matrix& matrix,const Matrix& other);
+    Matrix operator*(const Matrix& matrix, int scalar);
+    Matrix operator+(const Matrix& matrix, const Matrix& other);
+    Matrix operator-(const Matrix& matrix, const Matrix& other);
+    Matrix operator*(const Matrix& matrix, const Matrix& other);
