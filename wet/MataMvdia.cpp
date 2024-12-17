91e24a8fdc514e185572d2a09cf3d72409b1bdb3
KseniyaKara <kseniya.kara2@gmail.com>
2024-12-17 14:32:54 +0200

    add Film destructor

--- wet/MataMvdia.cpp
+++ wet/MataMvdia.cpp
@@ -2,3 +2,6 @@
 Film::Film(const std::string& filmName, const std::string& authorName, Matrix* filmFrames, unsigned int filmLength){}
 Film::Film(const Film& other){}
 Film& Film::operator=(const Film& s){}
+Film::~Film() {
+    delete[]filmFrames;
+}
