8bead84a38d6a46e6aad09ba5e3ffd8aa4f306ca
KseniyaKara <kseniya.kara2@gmail.com>
2024-12-17 14:58:24 +0200

    add operator "=" Film class

--- wet/MataMvdia.cpp
+++ wet/MataMvdia.cpp
@@ -1,4 +1,5 @@
 #include "MataMvdia.h"
+
 Film::Film(const std::string& filmName, const std::string& authorName, Matrix* filmFramesOther, unsigned int filmLength):
 filmName(filmName), authorName(authorName), filmLength(filmLength){
     filmFrames = new Matrix[filmLength];
@@ -13,6 +14,18 @@ authorName(other.authorName), filmLength(other.filmLength){
     filmFrames = new Matrix[filmLength];
     for (unsigned int i = 0; i < filmLength; i++) {
         filmFrames[i] = other.filmFrames[i];
+    }
+}
+
+Film& Film::operator=(const Film& s): filmName(s.filmName),
+authorName(s.authorName), filmLength(s.filmLength) {
+    if (this == &s){
+        return *this;
+    }
+    delete[] filmFrames;
+    filmFrames = new Matrix[filmLength];
+    for (int i = 0; i < filmLength; i++) {
+        filmFrames[i] = s.filmFrames[i];
     }
 }
 
