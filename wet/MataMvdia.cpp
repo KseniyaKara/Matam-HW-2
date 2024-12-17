458bd38db4178cecf9570d01ff45de07940cf822
KseniyaKara <kseniya.kara2@gmail.com>
2024-12-17 14:31:35 +0200

    add Film class declaration

--- wet/MataMvdia.cpp
+++ wet/MataMvdia.cpp
@@ -1,1 +1,4 @@
 #include "MataMvdia.h"
+Film::Film(const std::string& filmName, const std::string& authorName, Matrix* filmFrames, unsigned int filmLength){}
+Film::Film(const Film& other){}
+Film& Film::operator=(const Film& s){}
--- wet/MataMvdia.h
+++ wet/MataMvdia.h
@@ -1,5 +1,17 @@
-
+#include <string>
+#include "Matrix.h"
 #pragma once
 
+class Film {
+    private:
+        unsigned int filmLength = 0;
+        std::string filmName;
+        std::string authorName;
+        Matrix* filmFrames = nullptr;
+    public:
+        Film(const std::string& filmName, const std::string& authorName, Matrix* filmFrames, unsigned int filmLength);
+        Film(const Film& other);
+        Film& Film::operator=(const Film& s);
+        ~Film();
 
-
+};
