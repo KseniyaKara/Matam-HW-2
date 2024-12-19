a50d7623844c1942222d69bc28bc4c8b67ef02ae
EmilCodes <83508689+EmilCodes@users.noreply.github.com>
2024-12-19 17:32:01 +0200

    Delete wet/compile_and_run.sh

--- wet/compile_and_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#g++ -DNDEBUG -std=c++17 -Wall -pedantic-errors -Werror -g -o mtm_blockchain *.cpp
-g++ -DNDEBUG -std=c++17 -Wall -pedantic-errors -Werror -g -o MvidiaTest MataMvidia.cpp Matrix.cpp Utilities.cpp -I . ./tests/tests.cpp
-python3 ./run_tests.py ./tests/custom_tests.json ./MvidiaTest
